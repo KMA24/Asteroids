d82d22d9b0a3a27790de9738b963b70e8d692665
Kian Møller Andersen <kiand24@student.sdu.dk>
2026-05-10 23:10:41 +0200

    added dependencies to diffrent modules and commented

--- common/src/main/java/module-info.java
+++ common/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module common {
+module common { //The common module sends data and services to be used by other modules.
     exports dk.sdu.cbse.common.services;
     exports dk.sdu.cbse.common.data;
 }
